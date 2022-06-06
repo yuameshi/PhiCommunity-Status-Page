79378b2739fab9fd0883b62aa58ce1fa4510e90c
Yuameshi <64469437+Yuameshi@users.noreply.github.com>
2022-06-06 16:31:44 +0800

    Add files via upload

--- config.js
+++ config.js
@@ -1,26 +1,24 @@
 window.Config = {
-  SiteName: 'PhiCommunityの服务状态',
-  SiteUrl: '/',
-  ApiKeys: [
-    'm791467577-3073005bd6873c408735f51c', //GH
-    'm791749589-620f6b7b66cfd1405e877fcf', //GHC
-    'm791749604-4a000a686e1544f0fcde2583', //CF
-    'm791749618-e16051e510f188bfaf7b9c6d', //CFC
-    'm791749621-fb083fcf90f76be9a07d3bb0', //VC
-    'm791749666-1935f47a7b422f475418ee27', //VCC
-  ],
-  ShowLink: true,
-  CountDays: 60,
-
-  // 导航栏菜单
-  Navi: [
-    {
-      text: 'Yuameshiの窝',
-      url: 'https://www.yuameshi.top'
-    },
-    {
-      text: 'GitHub',
-      url: 'https://github.com/Yuameshi/'
-    }
-  ]
+	SiteName: 'PhiCommunityの服务状态',
+	SiteUrl: '/',
+	ApiKeys: [
+		'm791467577-3073005bd6873c408735f51c', //GH
+		'm791749589-620f6b7b66cfd1405e877fcf', //GHC
+		'm791749604-4a000a686e1544f0fcde2583', //CF
+		'm791749618-e16051e510f188bfaf7b9c6d', //CFC
+		'm791749621-fb083fcf90f76be9a07d3bb0', //VC
+		'm791749666-1935f47a7b422f475418ee27', //VCC
+	],
+	ShowLink: true,
+	CountDays: 60,
+	Navi: [
+		{
+			text: 'Yuameshiの窝',
+			url: 'https://www.yuameshi.top'
+		},
+		{
+			text: 'GitHub',
+			url: 'https://github.com/Yuameshi/'
+		}
+	]
 };
