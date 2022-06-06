5c9d0f447ae23f7dc289f20f319da7bbdc146384
Yuameshi <64469437+Yuameshi@users.noreply.github.com>
2022-06-06 16:27:30 +0800

    Add files via upload

--- config.js
+++ config.js
@@ -1,14 +1,6 @@
-// 配置
 window.Config = {
-
-  // 站点名
   SiteName: 'PhiCommunityの服务状态',
-
-  // 站点链接
   SiteUrl: '/',
-
-  // UptimeRobot Api Keys
-  // 支持 Monitor-Specific 和 Read-Only 两种 Api Key
   ApiKeys: [
     'm791467577-3073005bd6873c408735f51c', //GH
     'm791749589-620f6b7b66cfd1405e877fcf', //GHC
@@ -17,13 +9,7 @@ window.Config = {
     'm791749621-fb083fcf90f76be9a07d3bb0', //VC
     'm791749666-1935f47a7b422f475418ee27', //VCC
   ],
-
-  // 是否显示监测站点的链接
   ShowLink: true,
-
-  // 日志天数
-  // 虽然免费版说仅保存60天日志，但测试好像API可以获取90天的
-  // 不过时间不要设置太长，容易卡，接口请求也容易失败
   CountDays: 60,
 
   // 导航栏菜单
